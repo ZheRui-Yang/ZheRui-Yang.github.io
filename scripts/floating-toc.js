f7bc4aa0eb7e7a7c53d8d1eb193952f243e257b9
ZheRuiYang <610514207@gms.ndhu.edu.tw>
2021-07-19 21:29:56 +0800

    floating table of content is done.

--- scripts/floating-toc.js
+++ scripts/floating-toc.js
@@ -1,29 +1,44 @@
 const body = document.getElementsByTagName("body")[0];
-const sidetoc = document.getElementById("SideTOC");
+const sideTOC = document.getElementById("SideTOC");
+const tocBtn = document.getElementById("toc-button");
+const tocBackButton = document.getElementById("tocBackButton");
+const tocBackRect = document.getElementById("rect99");
+const span = document.getElementById("change-point");
+
+
+sideTOC.style.display = "none";
 
 body.onscroll = function() {
-  const span = document.getElementById("change-point");
-  const tocBtn = document.getElementById("toc-button");
-
-  if (document.documentElement.scrollTop>=span.offsetTop) {
-    tocBtn.style.display = "block";
-  } else {
-    
-    if (sidetoc.style.display == "block") {
-      toggleSideTOC();
+  if (document.documentElement.scrollTop > span.offsetTop) {
+    if (sideTOC.style.display == "none") {
+      tocBtn.style.display = "block";
     }
-
+  } else {
     tocBtn.style.display = "none";
   }
-
 };
 
-function toggleSideTOC() {
-  if (sidetoc.style.display == "none") {
-    sidetoc.style.display = "block";
+
+function sideTOCOn() {
+    sideTOC.style.display = "block";
     body.style.paddingLeft = "27%";
-  } else {
-    sidetoc.style.display = "none";
-    body.style.paddingLeft = "50px";
+    tocBtn.style.display = "none";
+    tocBackButton.style.display = "block";
+}
+
+function sideTOCOff() {
+  sideTOC.style.display = "none";
+  tocBackButton.style.display = "none";
+  body.style.paddingLeft = "50px";
+  if (document.documentElement.scrollTop > span.offsetTop) {
+    tocBtn.style.display = "block";
   }
+}
+
+function tocBackColorOn() {
+  tocBackRect.style.fill = "grey";
+}
+
+function tocBackColorOff() {
+  tocBackRect.style.fill = "#fff6d5";
 }
